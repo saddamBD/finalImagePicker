3b5c5ede03287657f62151e9e3cc5aa3d553cf97
Saddam Hossain <saddam@saddam.local>
2018-07-22 10:14:58 +0600

    initial commit

--- finalImagePicker/ViewController.swift
+++ finalImagePicker/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import ImagePicker
+import Lightbox
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,ImagePickerDelegate {
+    
+   
+    
+    @IBAction func clickPicker(_ sender: Any) {
+        
+        var setting = Configuration()
+        setting.doneButtonTitle = "exit"
+        setting.noImagesTitle = "Sorry! There are no images here!"
+        setting.recordLocation = true
+        setting.allowVideoSelection = true
+        
+        let myPicker = ImagePickerController(configuration: setting)
+        myPicker.delegate = self
+        
+        present(myPicker, animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        guard images.count > 0 else { return }
+        
+        let lightboxImages = images.map {
+            return LightboxImage(image: $0)
+        }
+        
+        let lightbox = LightboxController(images: lightboxImages, startIndex: 0)
+        imagePicker.present(lightbox, animated: true, completion: nil)
+    }
+    
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
+        imagePicker.dismiss(animated: true, completion: nil)
+
+        
+    }
 
 }
 
